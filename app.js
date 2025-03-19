3743ec8a3fd8001ae80cc9f11667be2f65d121c3
nlnu61 <navdeepkaur.lnu9@rdpolytech.ca>
2025-03-19 11:19:19 -0600

    Update

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.set('view engine', 'ejs');
 app.set('views', "./views");
 
 
+app.get('/home', (_req, res) =>{
+    res.send('Middleware Triggered');
+});
+
+//example of middleware function
+app.use((_req,_res, next) =>{
+    const time = Date();
+    console.log(time);
+
+    next();
+});
 
 
 app.listen(PORT, ()=>{
